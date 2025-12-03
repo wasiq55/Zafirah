b0381d0c7f4cba5b4da71672cae8d71d9f81988a
wasiq55 <wasiqwasiq5555@gmail.com>
2025-12-03 22:03:50 +0530

    Fix MongoDB connection for Render

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development: mongoose");
 
+// Use environment variable from Render
+const mongoURI = process.env.MONGODB_URI;
+
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => dbgr("connected"))
-  .catch((err) => dbgr(err));
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => dbgr("MongoDB connected"))
+  .catch((err) => dbgr("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
